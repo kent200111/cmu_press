1e015cfcdd69b2446926ba6279d6779cb02fefd9
kent200111 <s.maximo.rommelkent@cmu.edu.ph>
2024-02-26 13:26:10 +0800

    adminlte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- AdminLTE styles -->
-<link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
